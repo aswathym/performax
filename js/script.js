916cdf99def1f8603ecdd911ea73a1e03b8b59e2
Lavya R <lavya@qburst.com>
2016-04-05 10:09:07 +0530

    financial and accounting page navbar issue fixed

--- js/script.js
+++ js/script.js
@@ -48,5 +48,11 @@ $(document).ready(function() {
         $("#update_button").css('display','block');
         $(this).parent().parent().find('textarea').focus();
 	});
+    // $("#").click(function() {
+	//     $("#update_finance").prop('disabled',false);
+    //     $("#update_finance").css('height','150px');
+    //     $("#update_button").css('display','block');
+    //     $(this).parent().parent().find('textarea').focus();
+	// });
 
 });
