b45cebf3819bea79f029bf5fd2b4deb507bc9e02
Lavya R <lavya@qburst.com>
2016-04-12 15:56:47 +0530

    partial commit

--- js/script.js
+++ js/script.js
@@ -186,24 +186,17 @@ $(document).ready(function() {
         $(this).addClass('active')
     })
 
-    var max_fields      = 10; //maximum input boxes allowed
-    var wrapper         = $(".challenge_wrapper"); //Fields wrapper
-    var add_button      = $(".add_field_button"); //Add button ID
+    var wrapper         = $(".challenge_wrapper");
+    var add_button      = $(".add_field_button");
     var challenge_no    = 2;
-    var x = 1; //initlal text box count
-    $(add_button).click(function(e){ //on add input button click
+    $(add_button).click(function(e){
         e.preventDefault();
-        if(x < max_fields){ //max input box allowed
-            x++; //text box increment
-            challenge_no++;
-            $(wrapper).append('<div class="challenge"><label>Challenge '+challenge_no+' :</label><input type="text"><a href="#" class="remove_field">Remove </a></div>'); //add input box
-
-        }
+        challenge_no++;
+        $(wrapper).append('<div class="challenge"><label>Challenge '+challenge_no+' :</label><input type="text"><a href="#" class="remove_field">Remove </a></div>');
     });
 
    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-       e.preventDefault(); $(this).parent('div').remove(); x--;
-       challenge_no--;
+       e.preventDefault(); $(this).parent('div').remove();
    })
 
 
