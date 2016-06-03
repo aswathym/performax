e6d5e68b75b67c459ee463828980e6a12c9a5b17
Abin Thaha <abin@qburst.com>
2016-06-03 19:07:38 +0530

    Add more button for Assessor Details

--- js/assessor.js
+++ js/assessor.js
@@ -1,38 +1,6 @@
 $(document).ready(function() {
-	$('.h-slider').slider({
-        animate: 'fast',
-        min: 1,
-        max: 6,
-        value: 1,
-        step: 1,
-        slide: function(event, ui) {
-        var next_ul = $(this).siblings();
-           if (ui.value == 1) {
-            	next_ul.find('li').addClass('visibilityHidden');
-                next_ul.find('li:nth-child(1)').removeClass('visibilityHidden');
-            }
-            else if(ui.value == 2) {
-                next_ul.find('li').addClass('visibilityHidden');
-                next_ul.find('li:nth-child(2)').removeClass('visibilityHidden');
-            }
-            else if(ui.value == 3) {
-                next_ul.find('li').addClass('visibilityHidden');
-                next_ul.find('li:nth-child(3)').removeClass('visibilityHidden');
-            }
-            else if(ui.value == 4) {
-                next_ul.find('li').addClass('visibilityHidden');
-                next_ul.find('li:nth-child(4)').removeClass('visibilityHidden');
-            }
-            else if(ui.value == 5) {
-                next_ul.find('li').addClass('visibilityHidden');
-                next_ul.find('li:nth-child(5)').removeClass('visibilityHidden');
-            }
-            else if(ui.value == 6) {
-                next_ul.find('li').addClass('visibilityHidden');
-                next_ul.find('li:nth-child(6)').removeClass('visibilityHidden');
-            }
-        }
-    });
+	setSlider($('.h-slider'));
+    setSlider($('.h-slider-extra'));
     $('.pagination').on('click', 'li', function() {
         $('.pagination li').removeClass('disabledLi');
         $('.pagination li a').removeClass('disabledLi');
@@ -72,4 +40,71 @@ $(document).ready(function() {
             $(this).parent().parent().parent().find('.h-slider').removeClass('disabled-slider');
         }
     });
+
+    $('#analytics_and_insights .add_and_remove_button').on('click', 'button', function() {
+        if ($(this).parent().parent().find('.extra_rating').hasClass('displayNone')) {
+            $(this).parent().parent().find('.extra_rating').removeClass('displayNone');
+            $(this).removeClass('btn-primary');
+            $(this).addClass('btn-danger');
+            $(this).html("Remove");      
+        }
+        else {
+            $(this).parent().parent().find('.extra_rating').addClass('displayNone');
+            $(this).addClass('btn-primary');
+            $(this).removeClass('btn-danger');
+            $(this).html("Add More");
+            $(this).parent().parent().find('.extra_rating .rate li').addClass('visibilityHidden');
+            $(this).parent().parent().find('.extra_rating .rate li:first-child').removeClass('visibilityHidden');
+            setSlider($(this).parent().parent().find('.h-slider-extra'));
+            $('.extra-note-space').val("");
+        }
+    });
+    $('#analytics_and_insights').on('click', '.add_more_text', function() {
+        if ($(this).parent().parent().find('.extra-text-space').hasClass('displayNone')) {
+            $(this).parent().parent().find('.extra-text-space').removeClass('displayNone')
+            $(this).removeClass('btn-primary');
+            $(this).addClass('btn-danger');
+            $(this).html("Remove");  
+        }
+        else {
+            $(this).parent().parent().find('.extra-text-space').addClass('displayNone')
+            $(this).addClass('btn-primary');
+            $(this).removeClass('btn-danger');
+            $(this).html("Add More");
+             $('.extra-text-space').val("");
+        }
+    });
 });
+ 
+var setSlider = function(hslider) {
+    hslider.slider({
+        animate: 'fast',
+        min: 1,
+        max: 5,
+        value: 1,
+        step: 1,
+        slide: function(event, ui) {
+        var next_ul = $(this).siblings();
+           if (ui.value == 1) {
+                next_ul.find('li').addClass('visibilityHidden');
+                next_ul.find('li:nth-child(1)').removeClass('visibilityHidden');
+            }
+            else if(ui.value == 2) {
+                next_ul.find('li').addClass('visibilityHidden');
+                next_ul.find('li:nth-child(2)').removeClass('visibilityHidden');
+            }
+            else if(ui.value == 3) {
+                next_ul.find('li').addClass('visibilityHidden');
+                next_ul.find('li:nth-child(3)').removeClass('visibilityHidden');
+            }
+            else if(ui.value == 4) {
+                next_ul.find('li').addClass('visibilityHidden');
+                next_ul.find('li:nth-child(4)').removeClass('visibilityHidden');
+            }
+            else if(ui.value == 5) {
+                next_ul.find('li').addClass('visibilityHidden');
+                next_ul.find('li:nth-child(5)').removeClass('visibilityHidden');
+            }
+        }
+    });
+}
